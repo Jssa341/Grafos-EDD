872d6ce521cc283dc97fcb421ea1bee5a9f25da3
Jssa341 <jsalamanca288@gmail.com>
2024-10-21 17:48:31 -0500

    Update GrafoMatriz.h

--- GrafoMatriz.h
+++ GrafoMatriz.h
@@ -1,2 +1,27 @@
-hh
-ll
+#ifndef GRAFOMATRIZ_H
+#define GRAFOMATRIZ_H
+#include <vector>
+
+template<class T>
+class Grafo {
+    private:
+        std::vector<T> vertices;
+        int** aristas;
+    public:
+        Grafo();
+        void setVertices(std::vector<T> vertices);
+        void setAristas (int** aristas);
+        std::vector<T> getVertices();
+        int** getAristas();
+        int cantVertices();
+        int cantAristas();
+        int buscarVertice(T ver);
+        bool insertarVertice(T ver);
+        bool insertarArista(T ori, T des, int cos);
+        int buscarArista(T origen, T destino);
+        bool eliminarVertice(T ver);
+        bool eliminarArista (T origen, T destino);    
+};
+
+#include "Grafo.hxx"
+#endif
